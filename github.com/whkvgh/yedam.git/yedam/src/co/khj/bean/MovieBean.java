d536dd11ecacf8cb900bdaf55f97ea1780575e80
User <User@YD03-04>
2018-10-30 12:41:26 +0900

    Bean 완료

---
+++ github.com/whkvgh/yedam.git/yedam/src/co/khj/bean/MovieBean.java
@@ -0,0 +1,77 @@
+package co.khj.bean;
+
+public class MovieBean {
+	private String mgenre; // 영화 장르
+	private String mtitle; // 영화 제목
+	private String mdir; // 영화 감독
+	private String mdate; // 영화 제작일
+	private String mcountry;// 제작 국가
+	private String mactor; // 출연 배우
+	private String mauthor; // 작가
+	private String mstory; // 줄거리
+
+	public String getMgenre() {
+		return mgenre;
+	}
+
+	public void setMgenre(String mgenre) {
+		this.mgenre = mgenre;
+	}
+
+	public String getMtitle() {
+		return mtitle;
+	}
+
+	public void setMtitle(String mtitle) {
+		this.mtitle = mtitle;
+	}
+
+	public String getMdir() {
+		return mdir;
+	}
+
+	public void setMdir(String mdir) {
+		this.mdir = mdir;
+	}
+
+	public String getMdate() {
+		return mdate;
+	}
+
+	public void setMdate(String mdate) {
+		this.mdate = mdate;
+	}
+
+	public String getMcountry() {
+		return mcountry;
+	}
+
+	public void setMcountry(String mcountry) {
+		this.mcountry = mcountry;
+	}
+
+	public String getMactor() {
+		return mactor;
+	}
+
+	public void setMactor(String mactor) {
+		this.mactor = mactor;
+	}
+
+	public String getMauthor() {
+		return mauthor;
+	}
+
+	public void setMauthor(String mauthor) {
+		this.mauthor = mauthor;
+	}
+
+	public String getMstory() {
+		return mstory;
+	}
+
+	public void setMstory(String mstory) {
+		this.mstory = mstory;
+	}
+
+}
